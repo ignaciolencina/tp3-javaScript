395f6b8d30c5685cbcc49dd1f32b6e4f49e10c6f
Ignacio Lencina <nacholencina60@gmail.com>
2024-04-11 12:48:41 -0300

    Me rindo con el ejercicio 5

--- js/ejercicio5.js
+++ js/ejercicio5.js
@@ -1,1 +1,3 @@
 // 5- Definir una función que muestre información sobre una cadena de texto que se le pasa como argumento. A partir de la cadena que se le pasa, la función determina si esa cadena está formada sólo por mayúsculas, sólo por minúsculas o por una mezcla de ambas.
+
+// NO SE ME OCURRE COMO HACER PARA QUE COMPARE SI HAY O NO MAYUSCULAS Y DEMAS :(
